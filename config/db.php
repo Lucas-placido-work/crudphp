f9bc071636f8f6b8a45ee8c42180f667a6345356
Lucas Placido <sasori1233214@gmail.com>
2024-08-30 15:01:44 -0300

    [fix] sem mudanças relevantes

---
+++ config/db.php
@@ -0,0 +1,11 @@
+<?php
+require_once 'config.php';
+
+function getDbConnection() {
+    $conn = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+    if ($conn->connect_error) {
+        die("Conexão falhou: " . $conn->connect_error);
+    }
+    return $conn;
+}
+?>
