0df7731141c20c6e4177dbfd76f8db6402342611
Lucas Placido <sasori1233214@gmail.com>
2024-08-30 18:00:30 -0300

    [fix] agora cadastra e retorna contatos no banco

---
+++ includes/cadastroModel.php
@@ -0,0 +1,56 @@
+<?php
+require_once(__DIR__ . '/../config/db.php');
+
+class CadastroModel {
+    private $conn;
+
+    public function __construct() {
+        $this->conn = getDbConnection();
+    }
+
+    public function cadastrarContato($nome, $data_nascimento, $email, $profissao, $telefone, $celular, $celular_wpp, $notify_email, $notify_sms) {
+        $query = "INSERT INTO contatos (nome, data_nascimento, email, profissao, telefone, celular, celular_wpp, notify_email, notify_sms) 
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        
+        $stmt = $this->conn->prepare($query);
+        $stmt->bind_param("ssssiiiii", $nome, $data_nascimento, $email, $profissao, $telefone, $celular, $celular_wpp, $notify_email, $notify_sms);
+        
+        if ($stmt->execute()) {
+            $stmt->close();
+            $this->conn->close();
+            return true;
+        } else {
+            $stmt->close();
+            $this->conn->close();
+            return false;
+        }
+    }
+
+    public function listarContatos(){
+        $query = "SELECT * FROM contatos";
+        $stmt = $this->conn->prepare($query);
+        if ($stmt->execute()) {
+            $contatos = array();
+            $resultado = $stmt->get_result();
+            while ($linha = $resultado->fetch_assoc()) {
+                $contatos[] = $linha;
+            }
+            $stmt->close();
+            $this->conn->close();
+            return $contatos;
+        } else {
+            $stmt->close();
+            $this->conn->close();
+            return null;
+        }
+    }
+
+    public function editarContato(){
+
+    }
+
+    public function removerContato(){
+        
+    }
+}
+?>
